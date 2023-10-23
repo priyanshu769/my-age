555330195e63de75576a58c9ceb5dae283a0782f
priyanshu769 <prynsu@yahoo.com>
2023-10-23 17:17:09 +0530

    My Age 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [age, setAge] = useState("")
+  const [currentTime, setCurrentTime] = useState("Run")
+  const dob = new Date('1998-06-06T00:00:00').getTime()
+  const [timeDiff, setTimeDiff] = useState("Run")
+
+  useEffect(() => {
+    let myInterVal;
+    myInterVal = setInterval(() => {
+      setCurrentTime(new Date().getTime())
+      setTimeDiff(currentTime - dob)
+      setAge(timeDiff / 1000 / 60 / 60 / 24 / 365)
+    }, 200)
+    return () => clearInterval(myInterVal)
+  }, [currentTime, dob, timeDiff])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="verticalCenter">
+        <h1 className='time'>{age.toString().slice(0, 13)}</h1>
+      </div>
     </div>
   );
 }
